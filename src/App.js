f4259399b2443f71c6aa0a4008d682baa4114b4c
zee <zperezcanals@gmail.com>
2018-06-27 16:16:15 +0800

    basic components layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+import ImageLinkForm  from './components/imagelinkform/ImageLinkForm';
 import './App.css';
+import 'tachyons';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Navigation />
+      <Logo /> 
+      <ImageLinkForm />
+      {/*
+      <FaceRecognition /> */}
       </div>
     );
   }
