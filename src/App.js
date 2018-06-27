f833c5273a6db82d54af8498bb0947acc4fa9d00
zee <zperezcanals@gmail.com>
2018-06-27 17:14:29 +0800

    constructor and state set

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'tachyons';
 const particlesOpt = {
             		particles: {
             			number:{
-            				value: 80,
+            				value: 40,
             				density: {
             					enable: true,
             					value_area: 800
@@ -21,6 +21,21 @@ const particlesOpt = {
             	}
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state= {
+			input: '',
+		}
+	}
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	}
+
+	onButtonSubmit = () => {
+		console.log('click');
+	}
+
   render() {
     return (
       <div className="App">
@@ -28,7 +43,9 @@ class App extends Component {
       <Navigation />
       <Logo /> 
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+      onInputChange= {this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
       {/*
       <FaceRecognition /> */}
       </div>
