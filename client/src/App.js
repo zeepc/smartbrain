007139e851ad83db6acd479908c9af2979f089b3
zee <zperezcanals@gmail.com>
2018-07-19 14:32:18 -0400

    edit app.js

--- client/src/App.js
+++ client/src/App.js
@@ -42,6 +42,12 @@ class App extends Component {
 		}
 	}
 
+  componentDidmount() {
+    fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(console.log)
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
