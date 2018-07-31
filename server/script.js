4e3e92ecbd5d2a3fc8b224d445cb7eefe6101a06
zee <zperezcanals@gmail.com>
2018-07-31 16:15:06 -0400

    verify server info

--- server/script.js
+++ server/script.js
@@ -1,50 +1,24 @@
 const express =  require('express');
-
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
-const knex = require('knex');
+const cors = require('cors');
+const knex = require('knex')
 
 const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: '',
-    password: '',
     database: 'smart-brain'
   }
 });
 
-// db.select('*').from('users').then( data =>{
-// 	console.log(data)
-// });
-
-const database = {
-	users: [{
-		id: '123',
-		name: 'John',
-		email: 'john@gmail.com',
-		password: 'cookies',
-		entries: 0,
-		joined: new Date()
-	},
-		{
-		id: '124',
-		name: 'Zee',
-		email: 'zee@gmail.com',
-		password: 'chocolate',
-		entries: 0,
-		joined: new Date()
-	}
-	]
-}
-
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
+
+app.get('/', (req, res)=> {
 	res.send(database.users);
 })
 
@@ -55,8 +29,8 @@ app.post('/signin', (req, res) => {
 		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
 			if (isValid) {
 				return db.select('*').from('users')
-				.where('email', '=', req.body.email)
-				.then(user => {
+					.where('email', '=', req.body.email)
+					.then(user => {
 					res.json(user[0])
 				})
 				.catch(err => res.status(400).json('unable to register'))
@@ -96,20 +70,20 @@ app.post('/register', (req, res) => {
 	.catch(err => res.status(400).json('unable to register'))
 })
 
-app.get('/profile/:id', (req, res) =>{
+app.get('/profile/:id', (req, res) => {
 	 const { id } = req.params;
-	 db.select('*').from('users').where({id}).then(user => {
-	 	if(user.length){
+	 db.select('*').from('users').where({id})
+	 .then(user => {
+	 	if(user.length) {
 	 		res.json(user[0])
-	 	} else{
+	 	} else {
 	 		res.status(400).json('Could not find user!')
 	 	}
 	 })
 	 .catch(err => res.status(400).json('not found'))
 })
 
-
-app.put('/image', (req, res) =>{
+app.put('/image', (req, res) => {
 	const { id } = req.body;
 db('users').where('id', '=', id)
   .increment('entries', 1)
